d22bcebe3a950ea2b7daa7043afe49b8146b3fac
Tema Novikov <nai90@ya.ru>
2013-04-11 21:59:20 +0400

    Update 15.03.13.py
    ы

--- 15.03.13.py
+++ 15.03.13.py
@@ -1,25 +1,78 @@
+import math
 #класс фигуры
 class figure:
-#изначальное значение цвета-белый
-    color = "white"
+# Площадь фигуры.   
+    def ploshad(): 
+        pass
 #метод для изменнения цвета
-    def changecolor(self, newcolor ):
-        self.color = newcolor
-#Подкласс овал 
-class oval(figure):
-    def __init__(self, r):
+    def changecolor(self):
+        pass
+# Вывод параметров фигуры на экран.    
+    def show(self): 
+        pass
+    
+#Подкласс круг
+class krug(figure):
+    def __init__(self, r=0):
         self.luc = r
+    def show(self):
+        print("Радиус =", self.luc)
+    def ploshad (self):
+        return math.pi * float (self.luc) * float (self.luc)
+    def changecolor(self, newcolor = "" ):
+        self.color = newcolor
+        print ("Цвет - ", self.color)
+    
 #Подкласс прямоугольник
 class square(figure):
-    def __init__(self, a, b):
+    def __init__(self, a=0, b=0):
         self.dlina = a
         self.shirina = b
- 
-f = figure()
-o = oval(15)
- 
-s = square(25, 4)
-s.changecolor("green")
- 
-print(o.color, o.luc)
-print(s.color, s.dlina, s.shirina)
+        
+    def show(self):
+        print("Длина =", self.dlina)
+        print("Ширина =", self.shirina)
+        
+    def ploshad(self):
+        return float(self.dlina) * float(self.shirina)
+
+    def changecolor(self, newcolor = "" ):
+        self.color = newcolor
+        print ("Цвет - ", self.color)
+def Menu():
+    vopr = 0
+    print("Что вы хотите сделать?")
+    print("1 - создать круг;")
+    print("2 - создать прямоугольник;")
+    print("3 - выход.")
+    while (vopr == 0):
+        vopr = input("Ваш выбор: ")
+        try:
+            vopr = int(vopr)
+        except ValueError:
+            vopr = 0
+        if (vopr < 1 or vopr > 3):
+            print("Ошибочка");
+            vopr = 0
+    return vopr
+
+a = Menu()
+if (a == 1):
+    a = input("Введите радиус круга: ")
+    c = input ("Введите цвет круга: ")
+    s = krug(c)
+    s.changecolor (c)
+    f = krug(a)
+    f.show ()
+    print ("Площадь круга = ", f.ploshad())
+    
+else:
+    if (a == 2):
+        a = input("Введите длину прямоугольника: ")
+        b = input("Введите ширину прямоугольника: ")
+        c = input ("Введите цвет прямоугольника: ")
+        s = square (c)
+        s.changecolor (c)
+        f = square(a, b)
+        f.show()
+        print("Площадь прямоугольника =", f.ploshad())
